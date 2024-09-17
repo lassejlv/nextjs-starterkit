1651460a73a1eb3dc6055ea7a97ca15f12dc5e2d
lasse <77295879+lassejlv@users.noreply.github.com>
2024-09-17 14:19:15 +0200

    changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import { useQuery } from "@tanstack/react-query";
 export default function Page() {
   const response = useQuery({
     queryKey: ["users"],
+
     queryFn: async () => {
       return getUsersCount();
     },
@@ -22,7 +23,7 @@ export default function Page() {
         Typescript, Tailwind CSS & Drizzle ORM.
       </p>
 
-      <span className="mt-4 text-center text-lg text-zinc-500">
+      <span className="mt-4 text-center text-2xl text-zinc-500">
         There are a total of {response.isLoading ? "0" : response.data?.data}{" "}
         users in the database. <br />
         <span className="text-sm italic">
