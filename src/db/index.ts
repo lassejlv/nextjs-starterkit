7c24d36f0731021dac11733841c7b4ec4b52fcd2
lasse <77295879+lassejlv@users.noreply.github.com>
2024-09-18 08:21:39 +0200

    changes plzzz deploy now <3

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import { drizzle } from "drizzle-orm/postgres-js";
 import postgres from "postgres";
+
 import * as user from "./schemas/user";
 import * as emailVerify from "./schemas/emailVerify";
 
